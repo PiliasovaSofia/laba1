852835645b938dc0b96841c11f2a7f12f7c2beae
PiliasovaSofia <petruskad538@gmail.com>
2026-03-04 15:03:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-
-r = float(input("введите радиус круга (r): "))
+r = int(input("введите радиус круга :"))
 pi = 3.14159
-s = pi * ( r ** 2)
-print(f"Площадь круга (s) = {s:4f}")
+s = r*r**pi
+print(f'Площадь круга равна {s}')
+
+
+a = float(input("Введите a: "))
+b = float(input("введите  b: "))
+if a != 0:
+    x = -b / a
+    print("x =, x")
+elif b == 0:
+    print("решений очень много")
+else:
+    print("решений нет")
